da27f6b21853410addafd0b9f5c17cf5b57b21c1
Bozhdidar <bozidar.hristov@gmail.com>
2018-03-26 12:20:28 +0200

    Bring back auth user in form

--- src/components/Forms/AddResource.js
+++ src/components/Forms/AddResource.js
@@ -14,8 +14,8 @@ export default class AddResource extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // userId: this.props.authUser.uid,
-      userId: "example-uid",
+      userId: this.props.authUser.uid,
+      // userId: "example-uid",
       source: "tw",
       type: "pg",
       term: null
