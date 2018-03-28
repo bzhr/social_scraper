3141107d0027929461ddd6745b3505174e1acf2f
Bozhdidar <bozidar.hristov@gmail.com>
2018-03-28 17:21:04 +0200

    Firebase config from env vars

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,26 +1,13 @@
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
-const prodConfig = {
-  apiKey: "AIzaSyA43E7NwD4lHrw5Q97XZEvt_SbxnNEvDkA",
-  authDomain: "socialscraper-b61b6.firebaseapp.com",
-  databaseURL: "https://socialscraper-b61b6.firebaseio.com",
-  projectId: "socialscraper-b61b6",
-  storageBucket: "socialscraper-b61b6.appspot.com",
-  messagingSenderId: "619392522062"
+const config = {
+  apiKey: process.env.GATSBY_API_KEY,
+  authDomain: process.env.GATSBY_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_DATABASE_URL,
+  projectId: process.env.GATSBY_PROJECT_ID,
+  storageBucket: process.env.GATSBY_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID
 };
-
-const devConfig = {
-  apiKey: "AIzaSyCAyYi_TW3p2d7xSzFhskd6ju_jJWgyLeU",
-  authDomain: "socialscraperdev.firebaseapp.com",
-  databaseURL: "https://socialscraperdev.firebaseio.com",
-  projectId: "socialscraperdev",
-  storageBucket: "socialscraperdev.appspot.com",
-  messagingSenderId: "144987363582",
-};
-
-const config = process.env.NODE_ENV === 'production'
-  ? prodConfig
-  : devConfig;
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
@@ -28,12 +15,7 @@ if (!firebase.apps.length) {
 
 const db = firebase.database();
 const auth = firebase.auth();
-const twitterProvider = new firebase.auth.TwitterAuthProvider()
-const fbProvider = new firebase.auth.FacebookAuthProvider()
+const twitterProvider = new firebase.auth.TwitterAuthProvider();
+const fbProvider = new firebase.auth.FacebookAuthProvider();
 
-export {
-  db,
-  auth,
-  twitterProvider,
-  fbProvider
-};
+export { db, auth, twitterProvider, fbProvider };
