90ce703e3ea07f383b36efd4c8acfd78322a3b85
Bozhdidar <bozidar.hristov@gmail.com>
2018-04-01 12:23:47 +0200

    Test resource with 4 fields

--- src/components/Forms/AddResource.js
+++ src/components/Forms/AddResource.js
@@ -2,48 +2,40 @@ import React from "react";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
 
-import { Form, Button, Header } from "semantic-ui-react";
-
 function encode(data) {
   return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
 }
 
-export default class AddResource extends React.Component {
+export default class Contact extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      userId: this.props.authUser.uid,
-      source: "tw",
-      type: "pg",
-      term: null
-    };
+    this.state = {};
   }
 
-  handleSource = (e, { value }) => this.setState({ source: value });
-  handleType = (e, { value }) => this.setState({ type: value });
-  handleTerm = (e, { value }) => this.setState({ term: value });
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
 
   handleSubmit = e => {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "testForm", ...this.state })
+      body: encode({ "form-name": "testResource", ...this.state })
     })
-      .then(() => alert("Success!"), console.log(this.state))
+      .then(() => alert("Success!"))
       .catch(error => alert(error));
 
     e.preventDefault();
   };
 
   render() {
-    const { source, type, term } = this.state;
     return (
       <div>
-        <Header as="h2">Add new resource</Header>
+        <h1>Contact</h1>
         <form
-          name="testForm"
+          name="testResource"
           method="post"
           action="/thanks/"
           data-netlify="true"
@@ -55,57 +47,33 @@ export default class AddResource extends React.Component {
               Don’t fill this out: <input name="bot-field" />
             </label>
           </p>
-          <div name="source" inline>
-            <label>Source: </label>
-            <Form.Radio
-              name="source"
-              value="tw"
-              label="Twitter"
-              checked={source === "tw"}
-              onChange={this.handleSource}
-            />
-            <Form.Radio
-              name="source"
-              label="Facebook"
-              value="fb"
-              checked={source === "fb"}
-              onChange={this.handleSource}
-            />
-            <Form.Radio
-              name="source"
-              label="Instagram"
-              value="in"
-              checked={source === "in"}
-              onChange={this.handleSource}
-            />
-          </div>
-          <div name="type" inline>
-            <label>Type: </label>
-            <Form.Radio
-              name="type"
-              label="Page/Profile"
-              value="pg"
-              checked={type === "pg"}
-              onChange={this.handleType}
-            />
-            <Form.Radio
-              name="type"
-              label="Hashtag"
-              value="hs"
-              checked={type === "hs"}
-              onChange={this.handleType}
-            />
-          </div>
-          <Form.Input
-            required
-            name="term"
-            fluid
-            label="Term"
-            placeholder="Term"
-            onChange={this.handleTerm}
-          />
           <p>
-            <Button type="submit">Send</Button>
+            <label>
+              Your type:<br />
+            <input type="text" name="type" onChange={this.handleChange}/>
+            </label>
+          </p>
+          <p>
+            <label>
+              Your source:<br />
+              <input type="text" name="source" onChange={this.handleChange}/>
+            </label>
+          </p>
+          <p>
+            <label>
+              UID:<br />
+              <textarea name="uid" onChange={this.handleChange}/>
+            </label>
+          </p>
+
+          <p>
+            <label>
+              Term:<br />
+              <textarea name="term" onChange={this.handleChange}/>
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
           </p>
         </form>
       </div>
