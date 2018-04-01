e9bd1443a64e58ba6e4b468b18fba27b45cfa50a
Bozhdidar <bozidar.hristov@gmail.com>
2018-04-01 18:56:02 +0200

    Form name that corresponds to the fields.

--- src/components/Forms/AddResource.js
+++ src/components/Forms/AddResource.js
@@ -29,7 +29,7 @@ export default class AddResource extends React.Component {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "resource", ...this.state })
+      body: encode({ "form-name": "contact", ...this.state })
     })
       .then(() => alert("Success!"), console.log(this.state))
       .catch(error => alert(error));
