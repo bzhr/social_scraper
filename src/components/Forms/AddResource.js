46e2cae63866a99385676c7034889e25f598210f
Bozhdidar <bozidar.hristov@gmail.com>
2018-04-01 14:27:03 +0200

    Used terms for contact form that Netlify already recognizes

--- src/components/Forms/AddResource.js
+++ src/components/Forms/AddResource.js
@@ -2,40 +2,48 @@ import React from "react";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
 
+import { Form, Button, Header } from "semantic-ui-react";
+
 function encode(data) {
   return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
-export default class Contact extends React.Component {
+export default class AddResource extends React.Component {
+  // Email=UserId, Name=Source, message=Term
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: this.props.authUser.uid,
+      name: "tw",
+      term: null
+    };
   }
 
-  handleChange = (e) => {
-    this.setState({[e.target.name]: e.target.value});
-  }
+  handleSource = (e, { value }) => this.setState({ source: value });
+  handleType = (e, { value }) => this.setState({ type: value });
+  handleTerm = (e, { value }) => this.setState({ term: value });
 
   handleSubmit = e => {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "testResource", ...this.state })
+      body: encode({ "form-name": "resource", ...this.state })
     })
-      .then(() => alert("Success!"))
+      .then(() => alert("Success!"), console.log(this.state))
       .catch(error => alert(error));
 
     e.preventDefault();
   };
 
   render() {
+    const { source, type, term } = this.state;
     return (
       <div>
-        <h1>Contact</h1>
-        <form
-          name="testResource"
+        <Header as="h2">Add new resource</Header>
+        <Form
+          name="contact"
           method="post"
           action="/thanks/"
           data-netlify="true"
@@ -47,35 +55,59 @@ export default class Contact extends React.Component {
               Don’t fill this out: <input name="bot-field" />
             </label>
           </p>
+          <Form.Group name="name" inline>
+            <label>Source: </label>
+            <Form.Radio
+              name="name"
+              value="tw"
+              label="Twitter"
+              checked={name === "tw"}
+              onChange={this.handleSource}
+            />
+            <Form.Radio
+              name="name"
+              label="Facebook"
+              value="fb"
+              checked={name === "fb"}
+              onChange={this.handleSource}
+            />
+            <Form.Radio
+              name="name"
+              label="Instagram"
+              value="in"
+              checked={name === "in"}
+              onChange={this.handleSource}
+            />
+          </Form.Group>
+          {/*<Form.Group name="type" inline>
+            <label>Type: </label>
+            <Form.Radio
+              name="type"
+              label="Page/Profile"
+              value="pg"
+              checked={type === "pg"}
+              onChange={this.handleType}
+            />
+            <Form.Radio
+              name="type"
+              label="Hashtag"
+              value="hs"
+              checked={type === "hs"}
+              onChange={this.handleType}
+            />
+          </Form.Group>*/}
+          <Form.Input
+            required
+            name="message"
+            fluid
+            label="Term"
+            placeholder="Term"
+            onChange={this.handleTerm}
+          />
           <p>
-            <label>
-              Your type:<br />
-            <input type="text" name="type" onChange={this.handleChange}/>
-            </label>
+            <Button type="submit">Send</Button>
           </p>
-          <p>
-            <label>
-              Your source:<br />
-              <input type="text" name="source" onChange={this.handleChange}/>
-            </label>
-          </p>
-          <p>
-            <label>
-              UID:<br />
-              <textarea name="uid" onChange={this.handleChange}/>
-            </label>
-          </p>
-
-          <p>
-            <label>
-              Term:<br />
-              <textarea name="term" onChange={this.handleChange}/>
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+        </Form>
       </div>
     );
   }
