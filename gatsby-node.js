bd082b1c63491b19498e5aa63edfb8e5de1e4fb1
Bozhdidar <bozidar.hristov@gmail.com>
2018-04-16 13:18:06 +0200

    Remove both functions from node for debugging

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,52 +10,52 @@ const removeCsvExtension = filename => filename.replace(".csv", "");
 //   });
 // };
 
-exports.createPages = ({ boundActionCreators, graphql }) => {
-  const { createPage } = boundActionCreators;
-  return graphql(`
-    query ResourcesQuery {
-      allFile {
-        edges {
-          node {
-            relativePath
-            name
-          }
-        }
-      }
-    }
-  `).then(result => {
-    if (result.errors) {
-      return Promise.reject(result.errors);
-    }
-    result.data.allFile.edges.forEach(function(edge) {
-      const { node } = edge;
-      const urlPath = node.name
-      const nodeName = `all` + _.upperFirst(_.camelCase(`${node.name} Csv`));
-      console.log(nodeName);
-      return graphql(`
-        query IndexQuery {
-          ${nodeName} {
-            edges {
-              node {
-                retweet_count
-                favorite_count
-                tweet
-                tweet_time
-                medias_0_type
-                tweet_id
-              }
-            }
-          }
-        }
-      `).then(function(result) {
-        const component = `src/templates/resource.js`;
-        console.log(urlPath)
-        createPage({
-          path: urlPath,
-          component: path.resolve(component),
-          context: result
-        });
-      });
-    });
-  });
-};
+// exports.createPages = ({ boundActionCreators, graphql }) => {
+//   const { createPage } = boundActionCreators;
+//   return graphql(`
+//     query ResourcesQuery {
+//       allFile {
+//         edges {
+//           node {
+//             relativePath
+//             name
+//           }
+//         }
+//       }
+//     }
+//   `).then(result => {
+//     if (result.errors) {
+//       return Promise.reject(result.errors);
+//     }
+//     result.data.allFile.edges.forEach(function(edge) {
+//       const { node } = edge;
+//       const urlPath = node.name
+//       const nodeName = `all` + _.upperFirst(_.camelCase(`${node.name} Csv`));
+//       console.log(nodeName);
+//       return graphql(`
+//         query IndexQuery {
+//           ${nodeName} {
+//             edges {
+//               node {
+//                 retweet_count
+//                 favorite_count
+//                 tweet
+//                 tweet_time
+//                 medias_0_type
+//                 tweet_id
+//               }
+//             }
+//           }
+//         }
+//       `).then(function(result) {
+//         const component = `src/templates/resource.js`;
+//         console.log(urlPath)
+//         createPage({
+//           path: urlPath,
+//           component: path.resolve(component),
+//           context: result
+//         });
+//       });
+//     });
+//   });
+// };
