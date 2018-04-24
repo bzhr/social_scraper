38587519f4d694ccf9fa05b2be7c916703feda54
Bozhdidar <bozidar.hristov@gmail.com>
2018-04-24 14:24:29 +0200

    Improve sign in flow

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,25 +1,51 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, h1, Segment, Dimmer, Loader } from "semantic-ui-react";
 
-import { firebase, auth, db } from "../../firebase";
+import { firebase, auth } from "../../firebase";
 import * as routes from '../../constants/routes';
 
 class SignInPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+    };
+  }
+
   componentWillMount() {
     const { history } = this.props;
     firebase.auth.onAuthStateChanged(authUser => {
       if (authUser) {
         history.push(routes.ACCOUNT)
+        this.setState({isLoading: false})
+        
       }
     })
   }
   onTwitterSignIn = event => {
     auth.doSignInWithTwitter()
+    this.setState({isLoading: true})
   };
   render() {
+    if (auth.isLoggedIn()) {
+      return(
+        <h1>You're logged in</h1>
+      )
+    }
+    
+    if (this.state.isLoading) {
+      return (
+        <Segment>
+          <Dimmer active>
+            <Loader>Loading</Loader>
+          </Dimmer>
+        </Segment>
+      )
+    }
     return (
       <div>
-        <h1>SignIn</h1>
+        <h1>Sign in with your social media account.</h1>
         <Button onClick={this.onTwitterSignIn}>
           <div>Twitter Sign in</div>
         </Button>
