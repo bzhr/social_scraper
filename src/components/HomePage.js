9b2400bd82eda12845e67817550e7a3717d1bac7
Bozhdidar <bozidar.hristov@gmail.com>
2018-04-27 00:37:21 +0200

    Joined DB users with Form data

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,44 +1,78 @@
 import React from "react";
 import { Table } from "semantic-ui-react";
 import Link from "gatsby-link";
+import { db } from "../firebase";
+const _ = require("lodash");
 
-const HomePage = ({ data }) => {
-  const resources = data.allFile.edges;
-  console.log(data);
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>The Home Page lists all resources created by users.</p>
-      {resources ? <ResourceTable resources={resources} /> : null}
-    </div>
-  );
-};
+const fromObjectToList = object =>
+  object
+    ? Object.keys(object).map(key => ({ ...object[key], index: key }))
+    : [];
 
-const ResourceTable = ({ resources }) => (
+export default class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: []
+    };
+  }
+  componentWillMount() {
+    db
+      .onceGetUsers()
+      .then(snapshot =>
+        this.setState(() => ({ users: fromObjectToList(snapshot.val()) }))
+      );
+  }
+  render() {
+    const resources = this.props.data.allFile.edges;
+    const users = this.state.users
+    // console.log("Users", this.state.users, resources);
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>The Home Page lists all resources created by users.</p>
+        {(resources && users[0] ) ? (
+          <ResourceTable resources={resources} users={users} />
+        ) : null}
+      </div>
+    );
+  }
+}
+
+const ResourceTable = ({ resources, users }) => (
   <Table>
     <Table.Header>
       <Table.Row>
-        <Table.HeaderCell>UID</Table.HeaderCell>
+        <Table.HeaderCell>Profile Picture</Table.HeaderCell>
+        <Table.HeaderCell>Username</Table.HeaderCell>
         <Table.HeaderCell>Resource Name</Table.HeaderCell>
       </Table.Row>
     </Table.Header>
     <Table.Body>
       {resources.map(resource => (
-        <Table.Row key={resource.node.name}>
-          <Table.Cell>
-            {resource.node.relativePath.split("_")[1]}</Table.Cell>
-          <Table.Cell>
-
-          <Link
-            to={"/" + resource.node.name}
-          >
-            {resource.node.relativePath.split("_")[0].replace(".csv", "")}
-          </Link>
-          </Table.Cell>
-        </Table.Row>
+        <TableRow key={resource.node.name} resource={resource} users={users} />
       ))}
     </Table.Body>
   </Table>
 );
 
-export default HomePage;
+const TableRow = ({ resource, users }) => {
+  const idForm = resource.node.name.split("_")[1]
+  const findUser = (element) => element.index.toLowerCase() === idForm
+  const user = users.find(findUser)
+  console.log(user)
+  return (
+    <Table.Row >
+      <Table.Cell>
+        <img src={user.photoUrl} alt="profile photo"/>
+      </Table.Cell>
+      <Table.Cell>{user.username}</Table.Cell>
+      <Table.Cell>
+        <Link to={"/" + resource.node.name}>
+          {resource.node.name.split("_")[0].replace(".csv", "")}
+        </Link>
+      </Table.Cell>
+    </Table.Row>
+  );
+}
